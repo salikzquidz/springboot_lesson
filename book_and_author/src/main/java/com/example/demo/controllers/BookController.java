0164a079029b8fe43246c692462545442739cf43
salikzquidz <salikinbaik@gmail.com>
2023-06-07 12:17:38 +0800

    add simple view using thymeleaf

--- book_and_author/src/main/java/com/example/demo/controllers/BookController.java
+++ book_and_author/src/main/java/com/example/demo/controllers/BookController.java
@@ -3,7 +3,9 @@ package com.example.demo.controllers;
 import com.example.demo.service.BookService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class BookController {
@@ -16,6 +18,12 @@ public class BookController {
 
     @RequestMapping("/books")
     public String getBooks(Model model){
+        model.addAttribute("books", bookService.findAll());
+        return "books";
+    }
+
+    @RequestMapping("/book2")
+    public String getBook2(Model model){
         model.addAttribute("books", bookService.findAll());
         return "books";
     }
